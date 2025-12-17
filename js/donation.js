c9712482d0ae9deb2ed6fe94c45476eb2e912769
hasibBDK <hn91690434@gmail.com>
2025-12-17 20:39:16 +0600

    added js all with function

---
+++ js/donation.js
@@ -0,0 +1,76 @@
+document.getElementById('dontion-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    const donationInput = document.getElementById('donation-amount').value;
+    const donationAmount = parseFloat(donationInput);
+    const mainBalance = gettextElementvalueById('main-balance');
+    const amountDonated = gettextElementvalueById('amount-donated');
+    if(donationAmount > mainBalance){
+        alert('You do not have enough balance to make this donation.');
+        return;
+    }
+    else if(donationAmount <= 0 || isNaN(donationAmount)){
+        alert('Please enter a valid donation amount.');
+        return;
+    }
+    else{
+        const newMainBalance = mainBalance - donationAmount;
+        amountDonatedNew = amountDonated + donationAmount;
+        document.getElementById('main-balance').innerText = newMainBalance + ' BDT';
+        document.getElementById('amount-donated').innerText = amountDonatedNew + ' BDT';
+
+    }
+   
+    
+});
+
+
+
+document.getElementById('dontion-btn-feni').addEventListener('click', function(event) {
+    event.preventDefault();
+    const donationInputfeni = document.getElementById('donation-amount-feni').value;
+    const donationAmountfeni = parseFloat(donationInputfeni);
+    const mainBalance = gettextElementvalueById('main-balance');
+    const amountDonatedfeni = gettextElementvalueById('amount-donated-feni');
+    if(donationAmountfeni > mainBalance){
+        alert('You do not have enough balance to make this donation.');
+        return;
+    }
+    else if(donationAmountfeni <= 0 || isNaN(donationAmountfeni)){
+        alert('Please enter a valid donation amount.');
+        return;
+    }
+    else{
+        const newMainBalance = mainBalance - donationAmountfeni;
+        amountDonatedNewfeni = amountDonatedfeni + donationAmountfeni;
+        document.getElementById('main-balance').innerText = newMainBalance + ' BDT';
+        document.getElementById('amount-donated-feni').innerText = amountDonatedNewfeni + ' BDT';
+
+    }
+
+
+
+});
+
+
+document.getElementById('dontion-btn-movement').addEventListener('click', function(event) {
+    event.preventDefault();
+    const donationInputmovement = document.getElementById('donation-amount-movement').value;
+    const donationAmountmovement = parseFloat(donationInputmovement);
+    const mainBalance = gettextElementvalueById('main-balance');
+    const amountDonatedmovement = gettextElementvalueById('amount-donated-movement');
+    if(donationAmountmovement > mainBalance){
+        alert('You do not have enough balance to make this donation.');
+        return;
+    }
+    else if(donationAmountmovement <= 0 || isNaN(donationAmountmovement)){
+        alert('Please enter a valid donation amount.');
+        return;
+    }
+    else{
+        const newMainBalance = mainBalance - donationAmountmovement;
+        amountDonatedNewmovement = amountDonatedmovement + donationAmountmovement;
+        document.getElementById('main-balance').innerText = newMainBalance + ' BDT';
+        document.getElementById('amount-donated-movement').innerText = amountDonatedNewmovement + ' BDT';
+    }
+
+    });
---
+++ js/function.js
@@ -0,0 +1,6 @@
+function gettextElementvalueById(id){
+    const elementText = document.getElementById(id).innerText;
+    const elementValue = parseFloat(elementText);
+    return elementValue;
+}
+
